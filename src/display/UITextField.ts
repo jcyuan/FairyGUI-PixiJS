c062101fc47b7c3286b4e655e4fef05a9fbd18d3
J.C <jcyuan.space@gmail.com>
2018-07-03 19:50:29 +0800

    move the code part of Emoji checking from the deleted file 'extras.Text' into this class

--- src/display/UITextField.ts
+++ src/display/UITextField.ts
@@ -1,8 +1,56 @@
-///<reference path="../PIXI/extras/Text.ts" />
-
 namespace fgui {
 
-    export class UITextField extends PIXI.extras.Text implements IUIObject {
+    const isEmojiChar = function(charCode:number, nextCharCode:number):number {
+        const hs = charCode;
+        const nextCharValid = typeof nextCharCode === 'number' && !isNaN(nextCharCode) && nextCharCode > 0;
+
+        // surrogate pair
+        if (hs >= 0xd800 && hs <= 0xdbff)
+        {
+            if (nextCharValid)
+            {
+                const uc = ((hs - 0xd800) * 0x400) + (nextCharCode - 0xdc00) + 0x10000;
+
+                if (uc >= 0x1d000 && uc <= 0x1f77f)
+                {
+                    return 2;
+                }
+            }
+        }
+        // non surrogate
+        else if ((hs >= 0x2100 && hs <= 0x27ff)
+            || (hs >= 0x2B05 && hs <= 0x2b07)
+            || (hs >= 0x2934 && hs <= 0x2935)
+            || (hs >= 0x3297 && hs <= 0x3299)
+            || hs === 0xa9 || hs === 0xae || hs === 0x303d || hs === 0x3030
+            || hs === 0x2b55 || hs === 0x2b1c || hs === 0x2b1b
+            || hs === 0x2b50 || hs === 0x231a)
+        {
+            return 1;
+        }
+        else if (nextCharValid && (nextCharCode === 0x20e3 || nextCharCode === 0xfe0f || nextCharCode === 0xd83c))
+        {
+            return 2;
+        }
+
+        return 0;
+    }
+
+    //override for emoji test
+    PIXI.TextMetrics.canBreakChars = function(char: string, nextChar: string, token: string, index: number, breakWords?: boolean): boolean {
+        if(isEmojiChar(char.charCodeAt(0), nextChar && nextChar.charCodeAt(0)) == 2)
+            return false;
+        return true;
+    };
+
+    PIXI.TextMetrics.isBreakingSpace = function(char?: string): boolean {
+        if (typeof char !== 'string')
+            return false;
+        if(char === ' ') return false;  //not break by this
+        return (PIXI.TextMetrics._breakingSpaces.indexOf(char.charCodeAt(0)) >= 0);
+    };
+
+    export class UITextField extends PIXI.Text implements IUIObject {
 
         public UIOwner:GObject;
 
